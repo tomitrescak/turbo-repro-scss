132366836b992b5af2a6432900ee8f4e70f8cf34
Tomas Trescak <tomi.trescak@gmail.com>
2024-04-11 00:12:48 +1000

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
 /** Add your relevant code here for the issue to reproduce */
+import styles from "./page.module.scss";
+
 export default function Home() {
-  return null;
+  return (
+    <div className={`${styles.blue} green`}>
+      Hello
+      <div className="red">World</div>
+    </div>
+  );
 }
